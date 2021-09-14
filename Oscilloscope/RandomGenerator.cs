c9d2a0b30ee22941a11b7b831b71603cc4b2cca9
Pavel Trostianko <rengetsu@mail.ru>
2021-09-14 19:19:28 +0300

    RandomGenerator class added

---
+++ Oscilloscope/RandomGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oscilloscope
+{
+    public class RandomGenerator
+    {
+        //  Random Class
+        Random rand = new Random();
+
+
+        public double randomGen()
+        {
+            return rand.NextDouble();
+        }
+    }
+}
